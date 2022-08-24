068c8d488cb6452be893db9638ef98892f8f2e8f
lzlocki <lzlocki@poczta.fm>
2022-08-24 18:03:47 +0200

    Create daily service

---
+++ PackingTracer.Service/Services/DailyTargetService.cs
@@ -0,0 +1,23 @@
+﻿using PackingTracer.Data.HourlyTarget;
+using PackingTracer.Service.DbEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingTracer.Service.Services
+{
+    public class DailyTargetService : IDailyTargetService
+    {
+        private readonly dbEngine dbEngine = new dbEngine();
+
+        
+
+        public DayPicture GetDayResult(DateTime day)
+        {
+            var result = dbEngine.GetQuantityOfPackedSmc2MotorsByDay(day);
+            return result;
+        }
+    }
+}
---
+++ PackingTracer.Service/Services/IDailyTargetService.cs
@@ -0,0 +1,14 @@
+﻿using PackingTracer.Data.HourlyTarget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackingTracer.Service.Services
+{
+    internal interface IDailyTargetService
+    {
+        public DayPicture GetDayResult(DateTime day);
+    }
+}
