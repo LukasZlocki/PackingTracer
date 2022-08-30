ce583371d89635f065f989758dd92f029cabdc1b
lzlocki <lzlocki@poczta.fm>
2022-08-30 22:14:09 +0200

    fix null reference bug

--- PackingTracer.Data/PackingDay/PackedPerDay.cs
+++ PackingTracer.Data/PackingDay/PackedPerDay.cs
@@ -3,7 +3,13 @@
     public class PackedPerDay
     {
         public DateTime Day { get; set; }
-        public List<PackedUnit> PackedUnits { get; set; }
+        public List<PackedUnit>? PackedUnits { get; set; }
+
+        public PackedPerDay()
+        {
+            PackedUnits = new List<PackedUnit>();
+        }
+
     }
 }
         
