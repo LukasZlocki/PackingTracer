982fb1bce1b6e35ac3b85193f906a42a1558ed3a
lzlocki <lzlocki@poczta.fm>
2022-09-04 10:35:32 +0200

    Remodel sql query and models and db engine

---
+++ PackingTracer.Data/PackingDay/WorkDaySheet.cs
@@ -0,0 +1,15 @@
+﻿namespace PackingTracer.Data.PackingDay
+{
+    public class WorkDaySheet
+    {
+        public int Day { get; set; }
+        public List<PackedUnit> Packed { get; set; }
+
+        // Constructor
+        public WorkDaySheet()
+        {
+            Packed = new List<PackedUnit>();
+        }
+
+    }
+}
