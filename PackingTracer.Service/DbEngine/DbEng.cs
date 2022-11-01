cb796b7139fc227762c06fe6a0a4d5b8f27bd14e
lzlocki <lzlocki@poczta.fm>
2022-11-01 08:39:14 +0100

    Implement sql server settings into file

--- PackingTracer.Service/DbEngine/DbEng.cs
+++ PackingTracer.Service/DbEngine/DbEng.cs
@@ -1,13 +1,14 @@
 ﻿using System.Data;
 using Microsoft.Data.SqlClient;
 using PackingTracer.Data.PackingDay;
+using PackingTracer.Service.Services;
 
 namespace PackingTracer.Service.DbEngine
 {
     public class DbEng
     {
-        // ToDo: server config load from setup file
-        static string SERVER_CONFIG = "Data Source=DESKTOP-4AAFF58\\SQLEXPRESS;Initial Catalog=dbo.Unit_EventLog;Integrated Security=True; TrustServerCertificate=True";
+        //static string SERVER_CONFIG = "Data Source=DESKTOP-4AAFF58\\SQLEXPRESS;Initial Catalog=dbo.Unit_EventLog;Integrated Security=True; TrustServerCertificate=True";
+        string SERVER_CONFIG = ConfigurationService.LoadSqlConfigurationFile();
 
         /// <summary>
         /// Returns WorkDaySheet object by DateTime day
@@ -21,7 +22,6 @@ namespace PackingTracer.Service.DbEngine
             string _day = day.ToString("d");
             string _dayNext = day.AddDays(1).ToString("d");
 
-            // ToDo: Query load from file
             string queryString = "SELECT DATEPART(HOUR, Created) AS[WorkingHour], Created " +
                            "FROM[dbo.Unit_EventLog].dbo.dbPackingSMC " +
                                "WHERE StationID = 210 AND PostEventStateID = 2105 AND Created BETWEEN '" + _day + " 06:00:00' AND '" + _dayNext + " 05:59:59' " +
